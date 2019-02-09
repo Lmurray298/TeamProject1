bf9c0ba1378627eea60134797ccab319cf1ed349
Mike B <michael.byron@gmail.com>
2019-02-08 22:54:29 -0500

    Finalize working version with basic firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,33 @@
+date = Date.now();
+console.log(date);
+console.log(Unix_timestamp(date));
+
+var config = {
+  apiKey: "AIzaSyDtsTQcdT7ZtcZnbW0MeEP7StRujven73A",
+  authDomain: "weatherzip-3e2ba.firebaseapp.com",
+  databaseURL: "https://weatherzip-3e2ba.firebaseio.com",
+  projectId: "weatherzip-3e2ba",
+  storageBucket: "weatherzip-3e2ba.appspot.com",
+  messagingSenderId: "428960672799"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
 $(document).on('click', "#searchButton", function () {
   event.preventDefault();
   var zip = $('#searchInput').val().trim();
+//---------------------
+
+// Code for handling the push
+database.ref().push({
+  zip: zip,
+  dateAdded: firebase.database.ServerValue.TIMESTAMP
+ 
+});
+
+//-----------------
+
   console.log(zip);
   var queryURL = 'https://maps.googleapis.com/maps/api/geocode/json?&address=' + zip + '&key=AIzaSyAvM-cW9B1wJFyMwnNs_pThSi_n4vBQrQo';
   var weatherURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&APPID=c1430d8dc77fc1ff6d9ecd8f0b819cd6&units=imperial";
@@ -31,14 +58,7 @@ $(document).on('click', "#searchButton", function () {
     $.ajax(settings).done(function (response) {
       console.log(response);
       var weather = response.weather[0];
-      //function to convert the sunrise and sun set from unix time to military
-      function Unix_timestamp(t) {
-        var dt = new Date(t * 1000);
-        var hr = dt.getHours();
-        var m = "0" + dt.getMinutes();
-        var s = "0" + dt.getSeconds();
-        return hr + ':' + m.substr(-2) + ':' + s.substr(-2);
-      }
+      
       //variables created to hold associated pieces of weather information
       var weatherPic = weather.icon
       var icon = '<img src = "http://openweathermap.org/img/w/' + weatherPic + '.png"></img>';
@@ -68,5 +88,24 @@ function setMap(lat, lng) {
 //because the callback is to initMap, just set that up as generic holder map, and have it call setMap to do the work with the lat and lng variables.
 function initMap() {
   setMap(43.2081, -71.5376);
+}
+
+database.ref().on("child_added", function(snapshot) {
+  // storing the snapshot.val() in a variable for convenience
+  var sv = snapshot.val();
+  console.log(sv);
+  console.log(sv.dateAdded);
+      console.log(sv.zip);
+  $("#firebase").text('Last zip added from firebase: ' + sv.zip);
+  
+
+});
+//function to convert the sunrise and sun set from unix time to military
+function Unix_timestamp(t) {
+  var dt = new Date(t * 1000);
+  var hr = dt.getHours();
+  var m = "0" + dt.getMinutes();
+  var s = "0" + dt.getSeconds();
+  return hr + ':' + m.substr(-2) + ':' + s.substr(-2);
 }
 
